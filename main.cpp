94966c07cbb8def4fa35ae58de976697c15ca919
zheyuyang0515 <zheyuyang0515@gmail.com>
2020-06-05 20:48:45 -0400

    bug fix

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include "server/Server.h"
-#define IPADDR "192.168.1.111"
+#include "logger/Logger.h"
+#include "Util/Utility.h"
+//#define IPADDR "192.168.1.111"
+#define IPADDR "127.0.0.1"
 #define PORT 8686
-int main() {
-    Logger *logger = Logger::get_instance();
-    ThreadPool *pool = new ThreadPool(2, 4, 1);
+int main(int argc, char* argv[]) {
+    //init logger singleton(indicate log file directory and ignorance of severity)
+    Logger *logger = Logger::get_instance(LOG_DIR, INFO);
+    //init threadpool
+    ThreadPool *pool = new ThreadPool(4, 4, 1);
+    //init server
     Server *server = new Server(IPADDR, PORT, 1024, pool);
+    //start server
     server->Listen();
-    //std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
